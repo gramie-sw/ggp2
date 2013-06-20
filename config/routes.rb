01c9bca63d23a3f1b29c021eb293a63fc4b1006a
cmierich <cm.gramie@gmail.com>
2013-06-20 23:14:12 +0200

    Games Aggregates Teams controller inserted

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Ggp2::Application.routes.draw do
+
+
+  resources :games
+  resources :aggregates
+  resources :teams
+
+  root :to => "games#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
