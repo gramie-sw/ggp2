cac7d47362a8bbbdf2cc0aac95df30d0b7c39fd4
Christian Mierich <cmierich@gmail.com>
2014-02-09 19:10:20 +0100

    fixed sequence problem in teams factory

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 
   factory :team do
-    sequence(:country) { |n| %w[BR HR MX CM ES NL CL AU CO GR CI JP UY CR EN IT CH EC FR HN AR BA IR NG DE PT GH US BE DZ RU KR][n-1] }
+    sequence(:country) { |n| %w[BR HR MX CM ES NL CL AU CO GR CI JP UY CR EN IT CH EC FR HN AR BA IR NG DE PT GH US BE DZ RU KR][n % 32] }
   end
 end
