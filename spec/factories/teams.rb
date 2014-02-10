5a54534a6fb77a3944ddf2837104110625ba6346
Christian Mierich <cmierich@gmail.com>
2014-02-10 22:25:49 +0100

    updated seeds with aggregates

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 
   factory :team do
-    sequence(:country) { |n| %w[BR HR MX CM ES NL CL AU CO GR CI JP UY CR EN IT CH EC FR HN AR BA IR NG DE PT GH US BE DZ RU KR][n % 32] }
+    sequence(:country) { |(n)| %w[ZZ BR HR MX CM ES NL CL AU CO GR CI JP UY CR EN IT CH EC FR HN AR BA IR NG DE PT GH US BE DZ RU KR][n % 33] }
   end
 end
