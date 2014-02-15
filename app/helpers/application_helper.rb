6158ca5e814b9b7076a4fe0f423876245c408a59
Christian Mierich <cmierich@gmail.com>
2014-02-15 16:06:43 +0100

    removed_link_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,27 +12,4 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
-
-  #links
-  def add_link(path, text=t('general.add'), icon='fa fa-plus', only_icon: false,  html_options: {})
-    icon_link path, text, icon, only_icon, html_options: html_options
-  end
-
-  def back_link(path, text=t('general.back'), icon='fa fa-arrow-circle-o-left', only_icon: false, html_options: {})
-    icon_link path, text, icon, only_icon, html_options: html_options
-  end
-
-  def edit_link(path, text=t('general.edit'), icon='fa fa-pencil', only_icon: false, html_options:{})
-    icon_link path, text, icon, only_icon, html_options: html_options
-  end
-
-  def destroy_link(path, text=t('general.destroy'), icon='fa fa-trash-o', only_icon: false, confirm: confirm, html_options: {})
-    icon_link path, text, icon, only_icon, html_options: {method: :delete, data: {confirm: confirm}}.merge(html_options)
-  end
-
-  def icon_link(path, text, icon, only_icon, html_options: {})
-    link_to(path, html_options) do
-      "<i class=\"#{icon}\", title=\"#{text}\")}></i>#{only_icon ? '' :  " "  + text}".html_safe
-    end
-  end
 end
