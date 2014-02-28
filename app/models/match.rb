7c14782b6fe7da50902c55073efe9846d4c9b335
Christian Mierich <cmierich@gmail.com>
2014-02-28 19:29:45 +0100

    MatchesController tested

--- app/models/match.rb
+++ app/models/match.rb
@@ -11,8 +11,6 @@ class Match < ActiveRecord::Base
   validates :aggregate, presence: true
   validates :team_1, presence: {if: :team_1_id}
   validates :team_2, presence: {if: :team_2_id}
-  #validates :score_team_1, numericality: {only_integer: true}, inclusion: {in: 0..1000}, allow_nil: true
-  #validates :score_team_2, numericality: {only_integer: true}, inclusion: {in: 0..1000}, allow_nil: true
   validates :placeholder_team_1, presence: {if: lambda { |match| !match.team_1? }}, length: {minimum: 3, maximum: 64}, technical_name_allowed_chars: true, allow_blank: true
   validates :placeholder_team_2, presence: {if: lambda { |match| !match.team_2? }}, length: {minimum: 3, maximum: 64}, technical_name_allowed_chars: true, allow_blank: true
   validates :date, presence: true
--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -43,11 +43,44 @@ describe MatchesController do
 
   describe '#create' do
     context 'if successful' do
+      let(:aggregate) { create(:aggregate) }
+      let(:params) { {match: {position: 1, aggregate_id: aggregate.id, placeholder_team_1: 'Team1', placeholder_team_2: 'Team 2', date: Time.now}} }
 
+      it 'should redirect to index' do
+        post :create, params
+        response.should redirect_to matches_path
+      end
+
+      it 'should create match with values from params' do
+        post :create, params
+        assigns(:match).should_not be_a_new Match
+        assigns(:match).position.should eq 1
+      end
+
+      it 'should assign notice flash message' do
+        post :create, params
+        flash[:notice].should eq t('model.messages.created', model: assigns(:match).message_name)
+      end
     end
 
     context 'if failing' do
 
+      let(:params) { {match: {position: 1, aggregate_id: 2, placeholder_team_1: 'Team1', placeholder_team_2: 'Team 2', date: Time.now}} }
+
+      before :each do
+        Aggregate.any_instance.stub(:valid?).and_return(false)
+      end
+
+      it 'should render new' do
+        post :create, params
+        response.should render_template :new
+      end
+
+      it 'should assign @match' do
+        post :create, params
+        assigns(:match).should be_a_new Match
+        assigns(:match).position.should eq 1
+      end
     end
   end
 
@@ -72,7 +105,43 @@ describe MatchesController do
   end
 
   describe '#update' do
+    let(:match) { create(:match) }
 
+    context 'if successful' do
+
+      let(:params) { {id: match.to_param, match: {placeholder_team_1: 'Team 1 Updated', placeholder_team_2: 'Team 2 Updated'}}}
+
+      it 'should redirect to index' do
+        patch :update, params
+        response.should redirect_to matches_path
+      end
+
+      it 'should update match with given params' do
+        patch :update, params
+        assigns(:match).placeholder_team_1.should eq 'Team 1 Updated'
+        assigns(:match).placeholder_team_2.should eq 'Team 2 Updated'
+      end
+
+      it 'should assign flash notice' do
+        patch :update, params
+        flash[:notice].should eq t('model.messages.updated', model: assigns(:match).message_name)
+      end
+    end
+
+    context 'if failing' do
+
+      let(:params) { {id: match.to_param, match: {position: -1}}}
+
+      it 'should render edit' do
+        patch :update, params
+        response.should render_template :edit
+      end
+
+      it 'should assign @match' do
+        patch :update, params
+        assigns(:match).should eq match
+      end
+    end
   end
 
   describe '#destroy' do
