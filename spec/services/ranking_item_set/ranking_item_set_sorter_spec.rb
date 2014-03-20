22d4a2204df5a3ffb2169c64ec0199bdb9ffb8a2
Christian Mierich <cmierich@gmail.com>
2014-03-20 20:31:25 +0100

    RankingItemSetSorter sorts ranking_items over points correct_tips_count correct_tendency_tips_only_count user creation_date

--- spec/services/ranking_item_set/ranking_item_set_sorter_spec.rb
+++ spec/services/ranking_item_set/ranking_item_set_sorter_spec.rb
@@ -2,9 +2,11 @@ describe RankingItemSetSorter do
 
   let(:ranking_items) do
     [
-        build(:ranking_item, points: 5, user: build(:player, created_at: 3.days.ago)),
-        build(:ranking_item, points: 5, user: build(:player, created_at: 1.day.ago)),
-        build(:ranking_item, points: 6, user: build(:player, created_at: 2.days.ago))
+        build(:ranking_item, points: 5, correct_tips_count: 99, correct_tendency_tips_only_count: 99, user: build(:player, created_at: 8.days.ago)),
+        build(:ranking_item, points: 5, correct_tips_count: 99, correct_tendency_tips_only_count: 99, user: build(:player, created_at: 9.days.ago)),
+        build(:ranking_item, points: 6, correct_tips_count: 100, correct_tendency_tips_only_count: 100, user: build(:player, created_at: 10.days.ago)),
+        build(:ranking_item, points: 5, correct_tips_count: 100, correct_tendency_tips_only_count: 100, user: build(:player, created_at: 10.day.ago)),
+        build(:ranking_item, points: 5, correct_tips_count: 99, correct_tendency_tips_only_count: 100, user: build(:player, created_at: 10.days.ago))
     ]
 
   end
@@ -13,9 +15,11 @@ describe RankingItemSetSorter do
     it 'should sort ranking items by points and user creation date' do
       sorted_ranking_items = RankingItemSetSorter.sort ranking_items
 
-      sorted_ranking_items.first.should eq ranking_items.third
-      sorted_ranking_items.second.should eq ranking_items.first
-      sorted_ranking_items.third.should eq ranking_items.second
+      sorted_ranking_items[0].should eq ranking_items[2]
+      sorted_ranking_items[1].should eq ranking_items[3]
+      sorted_ranking_items[2].should eq ranking_items[4]
+      sorted_ranking_items[3].should eq ranking_items[1]
+      sorted_ranking_items[4].should eq ranking_items[0]
     end
   end
 end
