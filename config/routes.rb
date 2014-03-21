9ff828948c0398aae0f90fc5f318561c69a1133f
dgrawunder <dev.dgr@gmail.com>
2014-03-21 20:40:28 +0100

    refactored user data section in profile#show, outsourced form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Ggp2::Application.routes.draw do
   devise_scope :user do
     get 'sign_in' => 'devise/sessions#new'
     delete 'sign_out' => 'devise/sessions#destroy'
+    get 'edit_account' => 'registrations#edit'
     patch 'account' => 'registrations#update'
   end
 
