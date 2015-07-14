cc5ad1e3a72f238ff03aff93955052c93ac57729
dgrawunder <dev.dgr@gmail.com>
2015-07-14 21:06:06 +0200

    small renaming in ranking

--- app/domain/ranking/update/ranking_set_updater.rb
+++ app/domain/ranking/update/ranking_set_updater.rb
@@ -12,20 +12,20 @@ class RankingSetUpdater
     @champion_tip_ranking_set_factory = champion_tip_ranking_set_factory
   end
 
-  def update_tip_ranking_set match_id, previous_ranking_item
+  def update_tip_ranking_set match_id, previous_ranking_set
     tips = Tip.all_by_match_id(match_id)
-    update_ranking_set(match_id, tips, previous_ranking_item, tip_ranking_set_factory)
+    update_ranking_set(match_id, tips, previous_ranking_set, tip_ranking_set_factory)
   end
 
-  def update_champion_tip_ranking_set previous_ranking_item
+  def update_champion_tip_ranking_set previous_ranking_set
     champion_tips = ChampionTip.all
-    update_ranking_set(nil, champion_tips, previous_ranking_item, champion_tip_ranking_set_factory)
+    update_ranking_set(nil, champion_tips, previous_ranking_set, champion_tip_ranking_set_factory)
   end
 
   private
 
-  def update_ranking_set(match_id, tips, previous_ranking_item, factory)
-    ranking_set = factory.build(match_id, tips, previous_ranking_item)
+  def update_ranking_set(match_id, tips, previous_ranking_set, factory)
+    ranking_set = factory.build(match_id, tips, previous_ranking_set)
     ranking_set.save
     ranking_set
   end
