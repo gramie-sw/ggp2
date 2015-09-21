cd07535f772fa5b3cc0b6c2001d92572b65355db
dgrawunder <dev.dgr@gmail.com>
2015-09-21 21:56:11 +0200

    fixed wrong match sort issue in users_tips/show

--- spec/queries/tip_queries_spec.rb
+++ spec/queries/tip_queries_spec.rb
@@ -2,7 +2,7 @@ describe TipQueries do
 
   subject { Tip }
 
-  describe '#all_eager_by_user_id_and_aggregate_id_ordered_by_position' do
+  describe '#all_eager_by_user_id_and_aggregate_id_ordered' do
 
     let(:users) { [create(:player), create(:player)] }
 
@@ -17,20 +17,22 @@ describe TipQueries do
       ]
     end
 
+    let(:aggregates) { [create(:phase), create(:phase)] }
+
+    let(:matches) do
+      [
+          create(:match, aggregate: aggregates.first, position: 2),
+          create(:match, aggregate: aggregates.first, position: 1),
+          create(:match, aggregate: aggregates.second, position: 3),
+      ]
+    end
+
     describe 'if aggregate has no groups' do
 
-      let(:aggregates) { [create(:phase), create(:phase)] }
 
-      let(:matches) do
-        [
-            create(:match, aggregate: aggregates.first, position: 2),
-            create(:match, aggregate: aggregates.first, position: 1),
-            create(:match, aggregate: aggregates.second, position: 3),
-        ]
-      end
-
-      it 'returns all tips for given user_id and aggregate_id' do
-        actual_tips = subject.all_eager_by_user_id_and_aggregate_id_ordered_by_position(users.first.id, aggregates.first.id)
+      it 'returns all tips for given user_id and aggregate_id by given order' do
+        actual_tips =subject.all_eager_by_user_id_and_aggregate_id_ordered_by_position(
+            users.first.id, aggregates.first.id, order: 'matches.position')
         expect(actual_tips).to eq [tips.third, tips.first]
       end
     end
@@ -48,9 +50,19 @@ describe TipQueries do
         ]
       end
 
-      it 'returns all tips for given user_id and aggregate_id recursive' do
-        actual_tips = subject.all_eager_by_user_id_and_aggregate_id_ordered_by_position(users.first.id, phase.id)
+      it 'returns all tips for given user_id and aggregate_id recursive by given order' do
+        actual_tips = subject.all_eager_by_user_id_and_aggregate_id_ordered_by_position(
+            users.first.id, phase.id, order: 'matches.position')
         expect(actual_tips).to eq [tips.third, tips.first]
+      end
+    end
+
+    describe 'if no order is given' do
+
+      it 'returns tips ordered by match ids' do
+        actual_tips = subject.all_eager_by_user_id_and_aggregate_id_ordered_by_position(
+            users.first.id, aggregates.first.id)
+        expect(actual_tips).to eq [tips.first, tips.third]
       end
     end
   end
