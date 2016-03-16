6ea83353aa0ad880282fe5efb23d9b6b6e65f552
dgrawunder <dev.dgr@gmail.com>
2016-03-16 22:54:59 +0100

    updated to rails 4.2.6, renamed remove css extension from scss files

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,18 +1,5 @@
 module TeamsHelper
 
-  # <b>DEPRECATED:</b> Please use <tt>standard_flag_image_tag</tt> instead.
-  def standard_flag_image_tag_of team
-    if team.present?
-      image_tag 'blank.gif', class: "flag-16 flag-#{team.country.downcase}"
-    end
-  end
-
-  def standard_flag_image_tag team_abbreviation
-    if team_abbreviation.present?
-      image_tag 'blank.gif', class: "flag-16 flag-#{team_abbreviation.downcase}"
-    end
-  end
-
   def flag_image_tag(team_abbreviation, size: 16)
     if team_abbreviation.present?
       image_tag 'blank.gif', class: "flag flag-#{size} flag-#{team_abbreviation.downcase}"
--- spec/helpers/teams_helper_spec.rb
+++ spec/helpers/teams_helper_spec.rb
@@ -1,22 +1,5 @@
 describe TeamsHelper, :type => :helper do
 
-  describe 'standard_flag_image_tag' do
-
-    context 'when country is nil' do
-
-      it 'should return nil' do
-        expect(helper.standard_flag_image_tag(nil)).to be_nil
-      end
-    end
-
-    context 'when country is present' do
-
-      it 'should return img tag' do
-        expect(helper.standard_flag_image_tag('Germany')).to eq "<img class=\"flag-16 flag-germany\" src=\"/assets/blank.gif\" alt=\"Blank\" />"
-      end
-    end
-  end
-
   describe '#team_collection_for_select' do
 
     it 'returns collections array with all teams for select' do
