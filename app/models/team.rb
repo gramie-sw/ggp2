adbff44c8f5c63566658b7f9edad99df3db20648
dgrawunder <dev.dgr@gmail.com>
2016-04-01 16:00:38 +0200

    fixed teams country length

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < ActiveRecord::Base
   has_many :team_2_matches, class_name: :Match, foreign_key: :team_2_id, dependent: :restrict_with_exception
   has_many :champion_tips, dependent: :nullify
 
-  validates :country, presence: true, uniqueness: true, length: {maximum: 2}
+  validates :country, presence: true, uniqueness: true, length: {maximum: 16}
 
   scope :order_by_country_name_asc, -> { all.sort_by { |team| I18n.t(team.country, :scope => 'countries') } }
 
