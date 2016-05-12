6ce8f680a1c733e1e46a2b751c78866d49689393
dgrawunder <dev.dgr@gmail.com>
2016-05-12 23:07:39 +0200

    fixed wrong tournament title in email subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,6 +11,6 @@ class UserMailer < ActionMailer::Base
     @token = token
     @tournament_title = PropertyQueries.find_value(Property::TOURNAMENT_TITLE_KEY)
 
-    mail to: user.email
+    mail to: user.email, subject: I18n.t('user_mailer.user_signed_up.subject', tournament_title: @tournament_title)
   end
 end
