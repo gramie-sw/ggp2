baf3d57918926c8922ac41e68ef2c199601338f4
dgrawunder <dev.dgr@gmail.com>
2016-07-16 11:01:00 +0200

    updated to rails 2.4.7, devise, rspec and other gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,7 +59,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, :type => :controller
   config.include FactoryGirl::Syntax::Methods
   config.include ControllerMacros, type: :controller
